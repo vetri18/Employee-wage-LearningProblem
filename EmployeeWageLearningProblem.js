42cfa908d3d393b8113e07e03957d842b6023bb6
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-25 10:05:20 +0530

    CheckingEmployeePresentOrNot-JS UC 1

--- EmployeeWageLearningProblem.js
+++ EmployeeWageLearningProblem.js
@@ -1,1 +1,14 @@
 console.log("Welcome to the Employee Wage Computation problem in Java Script");
+
+let checkEmp = Math.floor((Math.random()*10)%2);
+
+const IS_ABSENT=0;
+
+if(checkEmp==IS_ABSENT)
+{
+    console.log("Employee is absent");
+}
+else
+{
+    console.log("Employee is present");
+}
